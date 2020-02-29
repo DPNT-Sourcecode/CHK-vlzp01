c23452411400e57393a8568420052e9878d76c33
sbx_user1051 <sbx_user1051@169.254.93.237>
2020-02-29 15:07:24 +0000

    Sat Feb 29 15:07:24 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,18 +2,16 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 import com.google.common.base.Strings;
+import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Multiset;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         if(Strings.isNullOrEmpty(skus)) return -1;
-        Lists.charactersOf(skus)
-                .stream()
-                .map()
+        HashMultiset<Character> productSet = HashMultiset.create();
+        productSet.addAll(Lists.charactersOf(skus));
+        productSet.s
         return 0;
     }
 }
-
-
-
-
--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -20,6 +20,3 @@ public class CheckoutSolutionTest {
         assertThat(solution.checkout("A"), equalTo(50));
     }
 }
-
-
-
