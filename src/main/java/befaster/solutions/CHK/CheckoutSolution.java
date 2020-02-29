4fa487458d2ebbb532ff4f575dabc846dd23f0af
sbx_user1051 <sbx_user1051@169.254.93.237>
2020-02-29 15:04:24 +0000

    Sat Feb 29 15:04:24 UTC 2020

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,14 +2,18 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         if(Strings.isNullOrEmpty(skus)) return -1;
-
+        Lists.charactersOf(skus)
+                .stream()
+                .map()
         return 0;
     }
 }
+
 
 
 
