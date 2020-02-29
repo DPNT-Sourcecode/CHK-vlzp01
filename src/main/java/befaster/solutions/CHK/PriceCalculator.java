37d1cf982d612405315b9ff6a46347dae1f3902f
sbx_user1051 <sbx_user1051@169.254.7.29>
2020-02-29 21:49:28 +0000

    Sat Feb 29 21:49:28 UTC 2020

--- src/main/java/befaster/solutions/CHK/PriceCalculator.java
+++ src/main/java/befaster/solutions/CHK/PriceCalculator.java
@@ -5,6 +5,12 @@ import java.util.Set;
 public class PriceCalculator {
 
     public Integer totalPrice(Set<Product> orderItems) {
-        return 0;
+        int totalPrice = 0;
+        for(Product orderItem : orderItems) {
+            PricingStrategy strategy = PricingStrategyFactory.getStrategy(orderItem.getSkuItem());
+            totalPrice += strategy.priceOf(orderItem.getQuantity(),orderItem.getSkuItem().getPrice());
+        }
+        return totalPrice;
     }
 }
+
--- src/main/java/befaster/solutions/CHK/PricingStrategyFactory.java
+++ src/main/java/befaster/solutions/CHK/PricingStrategyFactory.java
@@ -1,4 +1,22 @@
 package befaster.solutions.CHK;
 
 public class PricingStrategyFactory {
+
+    public static PricingStrategy getStrategy(SKUItem skuItem) {
+        PricingStrategy strategy;
+        switch (skuItem) {
+            case A:
+                strategy = new MultiPricingStrategy(new RegularPricingStrategy(),3,130);
+                break;
+            case B:
+                strategy = new MultiPricingStrategy(new RegularPricingStrategy(),2,45);
+            case C:
+            case D:
+            default:
+                strategy = new RegularPricingStrategy();
+                break;
+        }
+        return strategy;
+    }
 }
+
