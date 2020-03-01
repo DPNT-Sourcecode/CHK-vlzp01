1ed9cf9502ee4250a581664d47772bafe2900d7a
sbx_user1051 <sbx_user1051@169.254.215.13>
2020-03-01 18:45:24 +0000

    Sun Mar 01 18:45:24 UTC 2020

--- src/main/java/befaster/solutions/CHK/pricing/PriceCalculator.java
+++ src/main/java/befaster/solutions/CHK/pricing/PriceCalculator.java
@@ -4,8 +4,11 @@ import befaster.solutions.CHK.Product;
 import befaster.solutions.CHK.SKUItem;
 
 import java.util.Arrays;
+import java.util.EnumSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class PriceCalculator {
 
@@ -13,13 +16,20 @@ public class PriceCalculator {
         // apply basket offers
         currentOffers().forEach(offer -> offer.apply(orderItems));
         int totalPrice = 0;
+        EnumSet<SKUItem> compositeSKUItems = compositeSKUItems();
         // find price for non composite items
-        List<Product> nonCompositeProducts =
-        for(Product orderItem : orderItems) {
+        Predicate<Product> nonCompositeSKUs = product -> ! compositeSKUItems.contains(product.getSkuItem());
+        List<Product> nonCompositeProducts = filterProducts(orderItems,nonCompositeSKUs);
+        for(Product orderItem : nonCompositeProducts) {
             totalPrice += PricingStrategyFactory
                     .getStrategy(orderItem.getSkuItem())
                     .priceOf(orderItem.getQuantity(),orderItem.getSkuItem().getPrice());
         }
+        // find price for composite items
+        Predicate<Product> compositeSKUs = product -> compositeSKUItems.contains(product.getSkuItem());
+        List<Product> compositeProducts = filterProducts(orderItems,nonCompositeSKUs);
+        CompositeMultiPricingStrategy strategy = new CompositeMultiPricingStrategy(new RegularPricingStrategy(),3,45);
+        totalPrice += strategy.priceOf(compositeProducts);
         return totalPrice;
     }
 
@@ -32,8 +42,16 @@ public class PriceCalculator {
                 new BuyNGetSameProductOffer(SKUItem.U,3));
     }
 
-    private Set<SKUItem> compositeProducts() {
-        
+    private EnumSet<SKUItem> compositeSKUItems() {
+        return EnumSet.of(SKUItem.S,SKUItem.T,SKUItem.X,SKUItem.Y,SKUItem.Z);
+    }
+
+    private List<Product> filterProducts(Set<Product> orderItems, Predicate<Product> filter) {
+        return orderItems.stream()
+                .filter(filter)
+                .collect(Collectors.toList());
+
     }
 }
+
 
