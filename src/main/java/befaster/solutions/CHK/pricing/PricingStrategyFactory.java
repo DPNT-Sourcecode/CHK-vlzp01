1b400fd7ce2b55c375431c4e5dda950e5eaf1fcc
sbx_user1051 <sbx_user1051@169.254.83.93>
2020-03-01 12:59:01 +0000

    Sun Mar 01 12:59:01 UTC 2020

--- src/main/java/befaster/solutions/CHK/pricing/PricingStrategyFactory.java
+++ src/main/java/befaster/solutions/CHK/pricing/PricingStrategyFactory.java
@@ -2,29 +2,27 @@ package befaster.solutions.CHK.pricing;
 
 import befaster.solutions.CHK.SKUItem;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PricingStrategyFactory {
 
+    private  static final Map<SKUItem,PricingStrategy> map = new HashMap<>();
+
+    static {
+        map.put(SKUItem.A, new MultiPricingStrategy(
+                            new MultiPricingStrategy(new RegularPricingStrategy(), 3, 130),
+                            5,
+                            200));
+        map.put(SKUItem.B, new MultiPricingStrategy(new RegularPricingStrategy(),2,45));
+        map.put(SKUItem.C, new RegularPricingStrategy());
+        map.put(SKUItem.D, new RegularPricingStrategy());
+        map.put(SKUItem.F, new RegularPricingStrategy());
+
+    }
+
     public static PricingStrategy getStrategy(SKUItem skuItem) {
-        PricingStrategy strategy;
-        switch (skuItem) {
-            case A:
-                strategy = new MultiPricingStrategy(
-                                new MultiPricingStrategy(
-                                        new RegularPricingStrategy(),
-                                        3,
-                                        130),
-                                5,
-                                200);
-                break;
-            case B:
-                strategy = new MultiPricingStrategy(new RegularPricingStrategy(),2,45);
-                break;
-            case C:
-            case D:
-            default:
-                strategy = new RegularPricingStrategy();
-                break;
-        }
-        return strategy;
+        return map.get(skuItem);
     }
 }
+
