998a1c0626b8684b19aafaaaf29d4e43a1aaa187
sbx_user1051 <sbx_user1051@169.254.215.13>
2020-03-01 18:21:23 +0000

    Sun Mar 01 18:21:23 UTC 2020

--- src/main/java/befaster/solutions/CHK/pricing/CompositeMultiPricingStrategy.java
+++ src/main/java/befaster/solutions/CHK/pricing/CompositeMultiPricingStrategy.java
@@ -2,6 +2,7 @@ package befaster.solutions.CHK.pricing;
 
 import befaster.solutions.CHK.Product;
 
+import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
@@ -21,11 +22,29 @@ public class CompositeMultiPricingStrategy {
     public int priceOf(List<Product> products) {
         int totalPrice = 0;
         //sort in reverse pricing order,so that the order items grouping occurs in top priced items first
-        products.sort(Comparator.comparing(product -> {
-            return product.getSkuItem().getPrice();
-        }));
+        products.sort(Comparator.comparing((Product product) -> product.getSkuItem().getPrice()).reversed());
+        List<Product> scannedProducts = new ArrayList<>(products.size());
+        int scannedQuantity = 0;
+        // calculate discount price
+        for(Product product:products) {
+            scannedQuantity += product.getQuantity();
+            // if eligible for discount threshold, apply discount
+            if(scannedQuantity >= discountThreshold) {
+                scannedProducts.clear();
+                totalPrice += (scannedQuantity / discountThreshold) * discountPrice;
+                scannedQuantity = scannedQuantity % discountThreshold;
+                if(scannedQuantity > 0) {
+                    product.setQuantity(scannedQuantity);
+                    scannedProducts.add(product);
+                }
+            } else {
+                scannedProducts.add(product);
+            }
+        }
+        // calculate regular price for remaining
+        for(Product product:scannedProducts) {
+            totalPrice += strategy.priceOf(product.getQuantity(),product.getSkuItem().getPrice());
+        }
         return totalPrice;
     }
 }
-
-
